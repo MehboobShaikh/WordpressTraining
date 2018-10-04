783d7adc66dd4d9006b5b3997f1ac8c813306d93
MehboobShaikh <mehboobs@prdxn.com>
2018-10-04 19:26:54 +0530

    Responsive Done

--- Dummy/HTMLStructure/Task10/assets/js/script.js
+++ Dummy/HTMLStructure/Task10/assets/js/script.js
@@ -1,3 +1,8 @@
+/*
+	Author: Shaikh Mehboob Siraj
+	Version: 1.0
+*/
+
 /* Featured */
 
  $('.slider-for').slick({
@@ -8,7 +13,7 @@
 	arrows: true,
 	prevArrow: '<img class="slick-prev" src="assets/images/icons/left-arrow-angle.png">',
 	nextArrow: '<img class="slick-next" src="assets/images/icons/right-arrow-angle.png">',
-	asNavFor: '.slider-nav',
+	autoplay: true,
 	responsive: [
 	    {
 	      breakpoint: 850,
@@ -41,52 +46,70 @@
 
  $('.slider-for-specials').slick({
 	infinite: true,
+	initialSlide: 0,
 	slidesToShow: 4,
-	slidesToScroll: 1,
-	focusOnSelect: true,
-	arrows: false,
-	prevArrow: '<img class="slick-prev" src="assets/images/icons/left-arrow-angle.png">',
-	nextArrow: '<img class="slick-next" src="assets/images/icons/right-arrow-angle.png">',
-	asNavFor: '.slider-specials-nav',
 	responsive: [
 	    {
 	      breakpoint: 850,
 	      settings: {
-	      	arrows: false,
 	        slidesToShow: 3,
-	        slidesToScroll: 1
+	        slidesToScroll: 1,
+	        focusOnSelect: true,
+	        autoplay: true
 	      }
 	    },
 	    {
 	      breakpoint: 480,
 	      settings: {
-	      	arrows: false,
 	        slidesToShow: 2,
-	        slidesToScroll: 1
+	        slidesToScroll: 1,
+	        focusOnSelect: true,
+	        autoplay: true
 	      }
 	    },
 	    {
 	      breakpoint: 400,
 	      settings: {
-	      	arrows: false,
 	        slidesToShow: 1,
-	        slidesToScroll: 1
+	        slidesToScroll: 1,
+	        focusOnSelect: true,
+	        autoplay: true
 	      }
 	    }
 	  ]
  });
 
+ $(function(){
+  $('.hamburger').on('click',function(){
+  	// console.log($('.hamburger > i').hasClass('fa-bars'));
+  	if($('.hamburger > i').hasClass('fa-bars')){
+	  	$('.hamburger > i').removeClass('fa-bars');
+	    $('.hamburger > i').addClass('fa-times');
+	  	$( ".main-nav" ).slideDown("slow", function() {
+		    // Animation complete.
+		  });
+  	}else if($('.hamburger > i').hasClass('fa-times')){
+	  	$('.hamburger > i').removeClass('fa-times');
+	    $('.hamburger > i').addClass('fa-bars');
+	  	$( ".main-nav" ).slideUp("slow", function() {
+		    // Animation complete.
+		  });
+  	}
+  });
 
-/* Media Query */
+  $(window).resize(function() {
+  	var hamburgerDisplay = $('.hamburger').css(['display'])['display'];
 
-// function myFunction(x) {
-//     if (x.matches) {
-//     	console.log( $(slick.$slides[currentSlide]).data('index'));
-//     } else {
-//     	console.log( $(slick.$slides[currentSlide]).data('index'));
-//     }
-// }
+  	if(hamburgerDisplay == 'block'){
+	  	if($('.hamburger > i').hasClass('fa-bars')){
+		   	$( ".main-nav" ).css('display','none');
+	  	}else if($('.hamburger > i').hasClass('fa-times')){
+		   	$( ".main-nav" ).css('display','block');
+	  	}
+	  }else {
+	   	$( ".main-nav" ).css('display','inline-block');
+	  }
+	});
 
-// var x = window.matchMedia("(max-width: 700px)")
-// myFunction(x) // Call listener function at run time
-// x.addListener(myFunction) // Attach listener function on state changes
+
+});
