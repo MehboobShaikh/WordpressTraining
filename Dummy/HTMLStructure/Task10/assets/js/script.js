f3bef17fbc21791d2f7b19fadaf885d1ff2edb7e
MehboobShaikh <mehboobs@prdxn.com>
2018-10-05 11:09:45 +0530

    Client Slider Added

--- Dummy/HTMLStructure/Task10/assets/js/script.js
+++ Dummy/HTMLStructure/Task10/assets/js/script.js
@@ -46,9 +46,16 @@
 
  $('.slider-for-specials').slick({
 	infinite: true,
-	initialSlide: 0,
 	slidesToShow: 4,
 	responsive: [
+	    {
+	      breakpoint: 1500,
+	      settings: {
+			infinite: false,
+			initialSlide: 0,
+	        slidesToShow: 4
+	      }
+	    },
 	    {
 	      breakpoint: 850,
 	      settings: {
@@ -74,6 +81,42 @@
 	        slidesToScroll: 1,
 	        focusOnSelect: true,
 	        autoplay: true
+	      }
+	    }
+	  ]
+ });
+
+ /* Client */
+
+ $('.slider-for-client').slick({
+	infinite: true,
+	slidesToShow: 8,
+	slidesToScroll: 1,
+	focusOnSelect: true,
+	autoplay: true,
+	responsive: [
+	    {
+	      breakpoint: 850,
+	      settings: {
+	      	arrows: false,
+	        slidesToShow: 5,
+	        slidesToScroll: 1
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	      	arrows: false,
+	        slidesToShow: 4,
+	        slidesToScroll: 1
+	      }
+	    },
+	    {
+	      breakpoint: 400,
+	      settings: {
+	      	arrows: false,
+	        slidesToShow: 3,
+	        slidesToScroll: 1
 	      }
 	    }
 	  ]
